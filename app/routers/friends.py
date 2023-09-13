e15a40c5f5a8b6b96b2ece6cd394ea134cd82103
amirQuf <amir.ghasemian.ag@gmail.com>
2023-09-13 18:59:37 +0330

    fixing name for routers

--- app/routers/friends.py
+++ app/routers/friends.py
@@ -12,7 +12,7 @@ async def retrieve_all_Friends(skip: int = 0, limit: int = 100, db: Session = De
     friends = crud.get_friends(db, skip=skip, limit=limit)
     return friends
 
-@friends_router.get('/today/' )
+@friends_router.get('/today-birthdays' )
 async def retrieve_birthday_buddies(db: Session = Depends(get_db)):
     today = date.today()
     friends = crud.get_birthdays_buddy(db=db , today=today)
@@ -22,7 +22,7 @@ async def retrieve_birthday_buddies(db: Session = Depends(get_db)):
 
 
 
-@friends_router.get('/friend/{id}' , response_model=schemas.Friend,status_code=200)
+@friends_router.get('/friend/{id}' , response_model=schemas.Friend)
 async def retrieve_birthdate(id :int, db: Session = Depends(get_db)):
     friend = crud.get_friend(id=id, db=db)
     if friend :
@@ -37,4 +37,3 @@ async def add_friend(friend:schemas.FriendIn, db: Session = Depends(get_db) )-> 
         return new_friend
     except:
         raise HTTPException(detail="something is wrong", status_code=status.HTTP_400_BAD_REQUEST)
-
