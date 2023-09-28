f4328c4b924102b7d69851dfdc49d1a918cbce66
amirQuf <amir.ghasemian.ag@gmail.com>
2023-09-28 16:33:53 +0330

    add docstring

--- app/crud.py
+++ app/crud.py
@@ -1,34 +1,85 @@
-from sqlalchemy .orm import Session
-import  models ,schemas
+from sqlalchemy.orm import Session
+import models, schemas
 from datetime import date
-from fastapi import HTTPException, status
-from typing import List 
+from typing import List
 from models import Friend
-def get_friend(db:Session,id:int):
-    return db.query(models.Friend).filter(models.Friend.id ==id).first()
 
 
-def get_birthdays_buddy(db:Session , today:date):
-    result :List[Friend] = []
+def get_friend(db: Session, id: int):
+    """
+    Retrieve a list of friends with optional pagination.
+
+    Parameters:
+    - db (Session): SQLAlchemy database session.
+    - skip (int): Number of records to skip (default: 0).
+    - limit (int): Maximum number of records to retrieve (default: 100).
+
+    Returns:
+    - List[models.Friend]: List of friend records.
+    """
+    return db.query(models.Friend).filter(models.Friend.id == id).first()
+
+
+def get_birthdays_buddy(db: Session, today: date):
+    """
+    Retrieve friends with birthdays matching the provided date.
+
+    Parameters:
+    - db (Session): SQLAlchemy database session.
+    - today (date): Date for birthday comparison.
+
+    Returns:
+    - List[models.Friend]: List of friends with matching birthdays.
+    """
+    result: List[Friend] = []
     friends = db.query(models.Friend).all()
     for friend in friends:
-        if friend.birthday.day==today.day and friend.birthday.month ==today.month:
+        if friend.birthday.day == today.day and friend.birthday.month == today.month:
             result.append(friend)
     return result
 
 
-
-
 def get_friends(db: Session, skip: int = 0, limit: int = 100):
+    """
+    Retrieve a friend by their ID.
+
+    Parameters:
+    - db (Session): SQLAlchemy database session.
+    - id (int): ID of the friend to retrieve.
+
+    Returns:
+    - models.Friend: The friend record or None if not found.
+    """
     return db.query(models.Friend).offset(skip).limit(limit).all()
 
-def add_friend(db:Session, friend:schemas.Friend):
-    try:
-        db_friend = models.Friend(**friend.dict() )
-        db.add(db_friend)
-        db.commit()
-        db.refresh(db_friend)
-        return db_friend
-    except:
-        raise HTTPException(detail="something is wrong", status_code=status.HTTP_400_BAD_REQUEST) 
 
+def add_friend(db: Session, friend: schemas.Friend):
+    """
+    Add a new friend to the database.
+
+    Parameters:
+    - db (Session): SQLAlchemy database session.
+    - friend (schemas.Friend): Friend information to be added.
+
+    Returns:
+    - models.Friend: The newly added friend.
+    """
+    db_friend = models.Friend(**friend.dict())
+    db.add(db_friend)
+    db.commit()
+    db.refresh(db_friend)
+    return db_friend
+
+
+def search_friends(db: Session, query: str):
+    """
+    Search for a friend by their name.
+
+    Parameters:
+    - db (Session): SQLAlchemy database session.
+    - query (str): Name of the friend to search for.
+
+    Returns:
+    - models.Friend: The friend record or None if not found.
+    """
+    return db.query(models.Friend).filter(models.Friend.name.ilike(f"%{query}%")).all()
--- app/routers/friends.py
+++ app/routers/friends.py
@@ -1,43 +1,92 @@
-from fastapi import HTTPException , status ,Depends , APIRouter
+from fastapi import HTTPException, status, Depends, APIRouter
 import crud, schemas
 from dependencies import get_db
 from sqlalchemy.orm import Session
-from  datetime  import date
+from datetime import date
 
 
 friends_router = APIRouter()
 
-@friends_router.get('/friend/')
-async def retrieve_all_Friends(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
-    '''retrieve all the friends '''
+
+@friends_router.get("/friend/")
+async def retrieve_all_Friends(
+    skip: int = 0, limit: int = 100, db: Session = Depends(get_db)
+):
+    """
+    Retrieve a list of friends with optional pagination.
+
+    Parameters:
+    - skip (int): Number of records to skip (default: 0).
+    - limit (int): Maximum number of records to retrieve (default: 100).
+    - db (Session): SQLAlchemy database session.
+
+    Returns:
+    - List[schemas.Friend]: List of friend records.
+    """
     friends = crud.get_friends(db, skip=skip, limit=limit)
     return friends
 
-@friends_router.get('/today-birthdays' )
+
+@friends_router.get("/today-birthdays")
 async def retrieve_birthday_buddies(db: Session = Depends(get_db)):
-    '''retrieve all the birthday buddies for today'''
+    """
+    Retrieve friends with birthdays matching the current date.
+
+    Parameters:
+    - db (Session): SQLAlchemy database session.
+
+    Returns:
+    - Union[List[schemas.Friend], dict]: List of friends or a message indicating no birthdays.
+    """
     today = date.today()
-    friends = crud.get_birthdays_buddy(db=db , today=today)
+    friends = crud.get_birthdays_buddy(db=db, today=today)
     if not friends:
-        return {"message":"no birthday buddies for today.", "result":[]} 
+        return {"message": "no birthday buddies for today.", "result": []}
     return friends
 
 
+@friends_router.get("/friend/{id}", response_model=schemas.Friend)
+async def retrieve_birthdate(id: int, db: Session = Depends(get_db)):
+    """
+    Retrieve a friend by their ID.
 
-@friends_router.get('/friend/{id}' , response_model=schemas.Friend)
-async def retrieve_birthdate(id :int, db: Session = Depends(get_db)):
-    '''retrieve a single friend by id'''
+    Parameters:
+    - id (int): ID of the friend to retrieve.
+    - db (Session): SQLAlchemy database session.
+
+    Returns:
+    - schemas.Friend: The friend record.
+
+    Raises:
+    - HTTPException(404): If the friend does not exist.
+    """
     friend = crud.get_friend(id=id, db=db)
-    if friend :
+    if friend:
         return friend
-    raise HTTPException(detail="friend doesn't exist", status_code=status.HTTP_404_NOT_FOUND)
+    raise HTTPException(
+        detail="friend doesn't exist", status_code=status.HTTP_404_NOT_FOUND
+    )
 
 
-@friends_router.post('/friend/add',response_model=schemas.Friend)
-async def add_friend(friend:schemas.FriendIn, db: Session = Depends(get_db) )-> schemas.Friend:
-    '''add a new friend to the database'''
+@friends_router.post("/friend/add", response_model=schemas.Friend)
+async def add_friend(
+    friend: schemas.FriendIn, db: Session = Depends(get_db)
+) -> schemas.Friend:
+    """
+    Add a new friend to the database.
+
+    Parameters:
+    - friend (schemas.FriendIn): Friend information to be added.
+    - db (Session): SQLAlchemy database session.
+
+    Returns:
+    - schemas.Friend: The newly added friend.
+
+    Raises:
+    - HTTPException(400): If an error occurs during the addition process.
+    """
     try:
         new_friend = crud.add_friend(friend=friend, db=db)
         return new_friend
-    except:
-        raise HTTPException(detail="something is wrong", status_code=status.HTTP_400_BAD_REQUEST)
+    except Exception as e:
+        raise HTTPException(detail=str(e), status_code=status.HTTP_400_BAD_REQUEST)
