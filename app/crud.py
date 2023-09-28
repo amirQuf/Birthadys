39c6df43c8d6e5c5adf31ffea4cd79cd7bfd7385
amirQuf <amir.ghasemian.ag@gmail.com>
2023-09-28 15:59:49 +0330

    fixing

--- app/crud.py
+++ app/crud.py
@@ -1,15 +1,15 @@
 from sqlalchemy .orm import Session
 import  models ,schemas
 from datetime import date
-
+from fastapi import HTTPException, status
+from typing import List 
+from models import Friend
 def get_friend(db:Session,id:int):
     return db.query(models.Friend).filter(models.Friend.id ==id).first()
 
 
 def get_birthdays_buddy(db:Session , today:date):
-    print(today.day)
-    print(today.month)
-    result =[]
+    result :List[Friend] = []
     friends = db.query(models.Friend).all()
     for friend in friends:
         if friend.birthday.day==today.day and friend.birthday.month ==today.month:
@@ -23,10 +23,12 @@ def get_friends(db: Session, skip: int = 0, limit: int = 100):
     return db.query(models.Friend).offset(skip).limit(limit).all()
 
 def add_friend(db:Session, friend:schemas.Friend):
-    db_friend = models.Friend(**friend.dict() )
-    db.add(db_friend)
-    db.commit()
-    db.refresh(db_friend)
-    return db_friend
-
+    try:
+        db_friend = models.Friend(**friend.dict() )
+        db.add(db_friend)
+        db.commit()
+        db.refresh(db_friend)
+        return db_friend
+    except:
+        raise HTTPException(detail="something is wrong", status_code=status.HTTP_400_BAD_REQUEST) 
 
--- app/routers/friends.py
+++ app/routers/friends.py
@@ -9,11 +9,13 @@ friends_router = APIRouter()
 
 @friends_router.get('/friend/')
 async def retrieve_all_Friends(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
+    '''retrieve all the friends '''
     friends = crud.get_friends(db, skip=skip, limit=limit)
     return friends
 
 @friends_router.get('/today-birthdays' )
 async def retrieve_birthday_buddies(db: Session = Depends(get_db)):
+    '''retrieve all the birthday buddies for today'''
     today = date.today()
     friends = crud.get_birthdays_buddy(db=db , today=today)
     if not friends:
@@ -24,6 +26,7 @@ async def retrieve_birthday_buddies(db: Session = Depends(get_db)):
 
 @friends_router.get('/friend/{id}' , response_model=schemas.Friend)
 async def retrieve_birthdate(id :int, db: Session = Depends(get_db)):
+    '''retrieve a single friend by id'''
     friend = crud.get_friend(id=id, db=db)
     if friend :
         return friend
@@ -32,6 +35,7 @@ async def retrieve_birthdate(id :int, db: Session = Depends(get_db)):
 
 @friends_router.post('/friend/add',response_model=schemas.Friend)
 async def add_friend(friend:schemas.FriendIn, db: Session = Depends(get_db) )-> schemas.Friend:
+    '''add a new friend to the database'''
     try:
         new_friend = crud.add_friend(friend=friend, db=db)
         return new_friend
